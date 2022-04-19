a7ef8eb46591b6e02b503a3f4d18ac9eff383233
nandenongwang <35598777+nandenongwang@users.noreply.github.com>
2022-04-19 22:24:49 +0800

    Validators

--- client/src/main/java/org/apache/rocketmq/client/Validators.java
+++ client/src/main/java/org/apache/rocketmq/client/Validators.java
@@ -1,24 +1,8 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.rocketmq.client;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.UtilAll;
@@ -27,7 +11,7 @@ import org.apache.rocketmq.common.protocol.ResponseCode;
 import org.apache.rocketmq.common.topic.TopicValidator;
 
 /**
- * Common Validator
+ * 校验工具类
  */
 public class Validators {
     public static final String VALID_PATTERN_STR = "^[%|a-zA-Z0-9_-]+$";
@@ -36,7 +20,7 @@ public class Validators {
     public static final int TOPIC_MAX_LENGTH = 127;
 
     /**
-     * @return The resulting {@code String}
+     * 在原始串中找到第一个符合给定正则匹配规则的子串
      */
     public static String getGroupWithRegularExpression(String origin, String patternStr) {
         Pattern pattern = Pattern.compile(patternStr);
@@ -48,7 +32,7 @@ public class Validators {
     }
 
     /**
-     * Validate group
+     * 检验group格式 【不为空、长度小于127、大小写字母数字_-组成】
      */
     public static void checkGroup(String group) throws MQClientException {
         if (UtilAll.isBlank(group)) {
@@ -61,14 +45,14 @@ public class Validators {
 
         if (!regularExpressionMatcher(group, PATTERN)) {
             throw new MQClientException(String.format(
-                "the specified group[%s] contains illegal characters, allowing only %s", group,
-                VALID_PATTERN_STR), null);
+                    "the specified group[%s] contains illegal characters, allowing only %s", group,
+                    VALID_PATTERN_STR), null);
         }
 
     }
 
     /**
-     * @return <tt>true</tt> if, and only if, the entire origin sequence matches this matcher's pattern
+     * 校验字符串是否与给定正则pattern匹配
      */
     public static boolean regularExpressionMatcher(String origin, Pattern pattern) {
         if (pattern == null) {
@@ -78,8 +62,10 @@ public class Validators {
         return matcher.matches();
     }
 
-    public static void checkMessage(Message msg, DefaultMQProducer defaultMQProducer)
-        throws MQClientException {
+    /**
+     * 校验消息格式 【消息是否为空、消息体是否为空、长度是否符合要求、topic格式、topic是否允许发送】
+     */
+    public static void checkMessage(Message msg, DefaultMQProducer defaultMQProducer) throws MQClientException {
         if (null == msg) {
             throw new MQClientException(ResponseCode.MESSAGE_ILLEGAL, "the message is null");
         }
@@ -97,11 +83,13 @@ public class Validators {
         }
 
         if (msg.getBody().length > defaultMQProducer.getMaxMessageSize()) {
-            throw new MQClientException(ResponseCode.MESSAGE_ILLEGAL,
-                "the message body size over max value, MAX: " + defaultMQProducer.getMaxMessageSize());
+            throw new MQClientException(ResponseCode.MESSAGE_ILLEGAL, "the message body size over max value, MAX: " + defaultMQProducer.getMaxMessageSize());
         }
     }
 
+    /**
+     * 校验topic格式是否合法 【不为空、长度小于127、大小写字母数字_-组成】
+     */
     public static void checkTopic(String topic) throws MQClientException {
         if (UtilAll.isBlank(topic)) {
             throw new MQClientException("The specified topic is blank", null);
@@ -109,27 +97,31 @@ public class Validators {
 
         if (!regularExpressionMatcher(topic, PATTERN)) {
             throw new MQClientException(String.format(
-                "The specified topic[%s] contains illegal characters, allowing only %s", topic,
-                VALID_PATTERN_STR), null);
+                    "The specified topic[%s] contains illegal characters, allowing only %s", topic,
+                    VALID_PATTERN_STR), null);
         }
 
         if (topic.length() > TOPIC_MAX_LENGTH) {
             throw new MQClientException(
-                String.format("The specified topic is longer than topic max length %d.", TOPIC_MAX_LENGTH), null);
+                    String.format("The specified topic is longer than topic max length %d.", TOPIC_MAX_LENGTH), null);
         }
     }
 
+    /**
+     * 是否是系统topic 【包含在内置系统topic中或以rmq_sys_开头】
+     */
     public static void isSystemTopic(String topic) throws MQClientException {
         if (TopicValidator.isSystemTopic(topic)) {
-            throw new MQClientException(
-                    String.format("The topic[%s] is conflict with system topic.", topic), null);
+            throw new MQClientException(String.format("The topic[%s] is conflict with system topic.", topic), null);
         }
     }
 
+    /**
+     * 是否是不允许发送的topic 【默认 SCHEDULE_TOPIC_XXXX】
+     */
     public static void isNotAllowedSendTopic(String topic) throws MQClientException {
         if (TopicValidator.isNotAllowedSendTopic(topic)) {
-            throw new MQClientException(
-                    String.format("Sending message to topic[%s] is forbidden.", topic), null);
+            throw new MQClientException(String.format("Sending message to topic[%s] is forbidden.", topic), null);
         }
     }
 
