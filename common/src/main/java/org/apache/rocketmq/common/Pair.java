2519dc2ac73f8a020dda827acfa4a8448a5d63c8
laj6801 <1326391037@qq.com>
2022-04-21 17:01:25 +0800

    MQVersion Pair ServiceState ThreadFactoryImpl

--- common/src/main/java/org/apache/rocketmq/common/Pair.java
+++ common/src/main/java/org/apache/rocketmq/common/Pair.java
@@ -16,28 +16,16 @@
  */
 package org.apache.rocketmq.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 双返回值封装类
+ */
+@AllArgsConstructor
+@Data
 public class Pair<T1, T2> {
     private T1 object1;
+
     private T2 object2;
-
-    public Pair(T1 object1, T2 object2) {
-        this.object1 = object1;
-        this.object2 = object2;
-    }
-
-    public T1 getObject1() {
-        return object1;
-    }
-
-    public void setObject1(T1 object1) {
-        this.object1 = object1;
-    }
-
-    public T2 getObject2() {
-        return object2;
-    }
-
-    public void setObject2(T2 object2) {
-        this.object2 = object2;
-    }
 }
