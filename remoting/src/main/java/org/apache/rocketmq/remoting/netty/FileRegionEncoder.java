d5374a6d3621161725398b0c9ff2f33372724f2d
laj6801 <1326391037@qq.com>
2022-04-28 09:31:34 +0800

    FileRegionEncoder

--- remoting/src/main/java/org/apache/rocketmq/remoting/netty/FileRegionEncoder.java
+++ remoting/src/main/java/org/apache/rocketmq/remoting/netty/FileRegionEncoder.java
@@ -4,18 +4,17 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.FileRegion;
 import io.netty.handler.codec.MessageToByteEncoder;
-
 import io.netty.handler.ssl.SslHandler;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 
 /**
- * <p>
- *     By default, file region are directly transferred to socket channel which is known as zero copy. In case we need
- *     to encrypt transmission, data being sent should go through the {@link SslHandler}. This encoder ensures this
- *     process.
- * </p>
+ * tls需对传输内容做编码、无法通过零拷贝传输、需转换成堆bytebuf通过byte输出
+ * By default, file region are directly transferred to socket channel which is known as zero copy. In case we need
+ * to encrypt transmission, data being sent should go through the {@link SslHandler}. This encoder ensures this
+ * process.
  */
 public class FileRegionEncoder extends MessageToByteEncoder<FileRegion> {
 
@@ -24,13 +23,14 @@ public class FileRegionEncoder extends MessageToByteEncoder<FileRegion> {
      * can be handled by this encoder.
      *
      * @param ctx the {@link io.netty.channel.ChannelHandlerContext} which this {@link
-     * io.netty.handler.codec.MessageToByteEncoder} belongs to
+     *            io.netty.handler.codec.MessageToByteEncoder} belongs to
      * @param msg the message to encode
      * @param out the {@link io.netty.buffer.ByteBuf} into which the encoded message will be written
      * @throws Exception is thrown if an error occurs
      */
     @Override
     protected void encode(ChannelHandlerContext ctx, FileRegion msg, final ByteBuf out) throws Exception {
+        //重写写出方法、将源mappedbytebuffer通过堆bytebuf拷贝byte[]写出
         WritableByteChannel writableByteChannel = new WritableByteChannel() {
             @Override
             public int write(ByteBuffer src) throws IOException {
